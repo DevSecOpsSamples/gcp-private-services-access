741309df22d62d2b9bc4f216638bba69eca27c33
engel80 <prettyhip@gmail.com>
2023-01-07 02:11:56 +0900

    .

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -1,3 +1,12 @@
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+}
+
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-central1"
+}
+
 variable "stage" {
   description = "stage"
   type        = string
--- 03-memorystore-direct-peering/variables.tf
+++ 03-memorystore-direct-peering/variables.tf
@@ -1,13 +1,17 @@
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+}
+
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-central1"
+}
 variable "stage" {
   description = "stage"
   type        = string
   default     = "local"
 }
-variable "ip_cidr_range" {
-  description = "ip_cidr_range"
-  type        = string
-  default     = ""
-}
+
 variable "backend_bucket" {
   description = "backend bucket to save tfstate file"
   type        = string
